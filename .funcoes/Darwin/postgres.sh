a749f1d172f97cd2cc9d2fd790f7cc78e2a478fb
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-01-19 16:16:02 -0200

    Compatibilização com o meu dotfiles pessoal

--- .funcoes/Darwin/postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Autor: Paulo Jerônimo (pj@paulojeronimo, @paulojeronimo)
-#
-# Funções específicas para trabalho com o Postgres no OS X
-
-postgres_start() { sudo launchctl start com.edb.launchd.postgresql-9.3; }
-postgres_stop() { sudo launchctl stop com.edb.launchd.postgresql-9.3; }
--- .funcoes/Linux/Linux.sh
+++ .funcoes/Linux/Linux.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+# Informa o nome da distribuição Linux que está sendo executada
+distro() {
+  echo $(lsb_release -i | awk -F: '{print $2}')
+}
+
 # Desabilita regras de firewall no iptables.
 firewall_desabilitar() {
   sudo iptables -F
 }
+
+# Simula o pbcopy do OS X. (requer a instalação do pacote xsel)
+pbcopy() {
+  xsel --clipboard --input
+}
+
+# Simula o pbpaste do OS X. (requer a instalação do pacote xsel)
+pbpaste() {
+  xsel --clipboard --output
+}
+
+# vim: set tabstop=2 shiftwidth=2 expandtab:
